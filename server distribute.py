4472134833f33f51dda19ed83590c2e1fcd127e0
Puzzl3d3d <olivernorval08@outlook.com>
2023-09-10 21:05:07 +0100

    Refactored code structure

--- server distribute.py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-
-code = urllib.request.urlopen('https://raw.githubusercontent.com/Puzzl3d3d/discord-2p-budget/main/server.py').read().decode('utf8')
-
-exec(compile(code, "__str__", "exec"))
